082ce34f96ccc99b7347412bf0391cc0eb130e4e
“GodwinEbikwo” <godwin.a.ebikwo@gmail.com>
2022-03-27 16:54:25 +0100

    woza

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
 import "@styles/main.css";
 import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   return (
-    <AnimatePresence exitBeforeEnter>
-      <Component {...pageProps} key={router.asPath} />
-    </AnimatePresence>
+    <>
+      <Head>
+        <title>Godwin Ebikwo - Personal Site</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          property="og:title"
+          content="Godwin Ebikwo - Personal Site"
+          key="title"
+        />
+        <meta name="description" content="" />
+      </Head>
+      <AnimatePresence exitBeforeEnter>
+        <Component {...pageProps} key={router.asPath} />
+      </AnimatePresence>
+    </>
   );
 }
